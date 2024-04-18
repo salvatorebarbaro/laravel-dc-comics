ca72e03fb647322030e0cb104875bb3ff91ede74
salvatore_barbaro <salvatore.ilbarbaro97@gmail.com>
2024-04-18 12:35:19 +0200

    inserimento gestione inserimento dati anche nel create

--- resources/views/comics/create.blade.php
+++ resources/views/comics/create.blade.php
@@ -13,14 +13,36 @@ Dobbiamo inserirlo in ogni form. -->
             <label for="title" class="form-label text-uppercase">Nome del fumetto</label>
             <!-- name campo da inserire perchè ci facilità la gestione del campo come per esempio in questo caso il nome -->
             <!-- required campo inserito per dire che noi dobbiamo avere per forza questo campo validato -->
-            <input type="text" class="form-control" id="name" name="title" value="{{ old ('title' )}}">
+            <input type="text" class="form-control @error('title')is-invalid @enderror" id="name" name="title" value="{{ old ('title' )}}">
             <!-- required -->
+                <!-- inizio gestione errore del titlo con relativo messaggio sotto al campo  giusto -->
+                @error('title')
+                <!-- direttiva error di blade -->
+                <div class="alert alert-danger ">
+                    {{$message}}
+                    <!-- questo campo si prende l'errore che abbiamo inserito nella nostra funzione validate nel controller -->
+                </div>
+
+                @enderror
+                <!-- fine -->
         </div>
         <div class="mb-3">
             <label for="description" class="form-label text-uppercase">descriozione del fumetto</label>
             <!-- name campo da inserire perchè ci facilità la gestione del campo come per esempio in questo caso il nome -->
             <!-- old description è stato inserito perchè facendo cio i dati inseriti dall'utente anche se sbagliati gli vengono fatti rivedere -->
-            <textarea type="text" class="form-control" id="description" name="description" > {{ old ('description' )}}</textarea>
+            <textarea type="text" class="form-control @error('description')is-invalid @enderror" id="description" name="description" > {{ old ('description' )}}</textarea>
+
+            <!-- inizio gestione errore del titlo con relativo messaggio sotto al campo  giusto -->
+            @error('description')
+            <!-- direttiva error di blade -->
+            <div class="alert alert-danger ">
+                {{$message}}
+                <!-- questo campo si prende l'errore che abbiamo inserito nella nostra funzione validate nel controller -->
+            </div>
+    
+            @enderror
+            <!-- fine -->
+
         </div>
         <div class="mb-3">
             <label for="thumb" class="form-label text-uppercase ">link dell immagine del fumetto</label>
@@ -30,37 +52,110 @@ Dobbiamo inserirlo in ogni form. -->
         <div class="mb-3">
             <label for="price" class="form-label text-uppercase ">prezzo del fumetto</label>
             <!-- name campo da inserire perchè ci facilità la gestione del campo come per esempio in questo caso il nome -->
-            <input type="text" class="form-control"  id="price" name="price" value="number" value="{{ old (' price' )}}">
+            <input type="text" class="form-control @error('price')is-invalid @enderror"  id="price" name="price" value="number" value="{{ old (' price' )}}">
+
+            <!-- inizio gestione errore del titlo con relativo messaggio sotto al campo  giusto -->
+            @error('price')
+            <!-- direttiva error di blade -->
+            <div class="alert alert-danger ">
+                {{$message}}
+                <!-- questo campo si prende l'errore che abbiamo inserito nella nostra funzione validate nel controller -->
+            </div>
+    
+            @enderror
+            <!-- fine -->
+
         </div>
         <div class="mb-3">
             <label for="series" class="form-label text-uppercase">serie del fumetto</label>
             <!-- name campo da inserire perchè ci facilità la gestione del campo come per esempio in questo caso il nome -->
-            <input type="text" class="form-control" id="series" name="series" value="{{ old ( 'series' )}}">
+            <input type="text" class="form-control @error('series')is-invalid @enderror" id="series" name="series" value="{{ old ( 'series' )}}">
+        
+             <!-- inizio gestione errore del titlo con relativo messaggio sotto al campo  giusto -->
+             @error('series')
+            <!-- direttiva error di blade -->
+            <div class="alert alert-danger ">
+                {{$message}}
+                <!-- questo campo si prende l'errore che abbiamo inserito nella nostra funzione validate nel controller -->
+            </div>
+    
+            @enderror
+            <!-- fine -->
+
+        
         </div>
         <!--  -->
         <div class="mb-3">
             <label for="sale_date" class="form-label text-uppercase">data di vendità</label>
             <!-- name campo da inserire perchè ci facilità la gestione del campo come per esempio in questo caso il nome -->
-            <input type="date" class="form-control" id="sale_date" name="sale_date" value="{{ old (' sale_date' )}}">
+            <input type="date" class="form-control @error('sale_date')is-invalid @enderror" id="sale_date" name="sale_date" value="{{ old (' sale_date' )}}">
+        
+            <!-- inizio gestione errore del titlo con relativo messaggio sotto al campo  giusto -->
+            @error('sale_date')
+            <!-- direttiva error di blade -->
+            <div class="alert alert-danger ">
+                {{$message}}
+                <!-- questo campo si prende l'errore che abbiamo inserito nella nostra funzione validate nel controller -->
+            </div>
+    
+            @enderror
+            <!-- fine -->
+        
         </div>
         <div class="mb-3">
             <label for="type" class="form-label text-uppercase">tipo</label>
             <!-- name campo da inserire perchè ci facilità la gestione del campo come per esempio in questo caso il nome -->
-            <input type="text" class="form-control" id="type" name="type" value="{{ old (' type' )}}">
+            <input type="text" class="form-control @error('type')is-invalid @enderror" id="type" name="type" value="{{ old (' type' )}}">
+        
+            <!-- inizio gestione errore del titlo con relativo messaggio sotto al campo  giusto -->
+            @error('type')
+            <!-- direttiva error di blade -->
+            <div class="alert alert-danger ">
+                {{$message}}
+                <!-- questo campo si prende l'errore che abbiamo inserito nella nostra funzione validate nel controller -->
+            </div>
+    
+            @enderror
+            <!-- fine -->
+        
         </div>
         <div class="mb-3">
             <label for="artists" class="form-label text-uppercase">artisti</label>
             <!-- name campo da inserire perchè ci facilità la gestione del campo come per esempio in questo caso il nome -->
-            <input type="text" class="form-control" id="artists" name="artists" value="{{ old (' artists' )}}">
+            <input type="text" class="form-control   @error('artists')is-invalid @enderror" id="artists" name="artists" value="{{ old (' artists' )}}">
+        
+            <!-- inizio gestione errore del titlo con relativo messaggio sotto al campo  giusto -->
+            @error('artists')
+            <!-- direttiva error di blade -->
+            <div class="alert alert-danger ">
+                {{$message}}
+                <!-- questo campo si prende l'errore che abbiamo inserito nella nostra funzione validate nel controller -->
+            </div>
+    
+            @enderror
+            <!-- fine -->
+        
         </div>
         <div class="mb-3">
             <label for="writers" class="form-label text-uppercase">serie del fumetto</label>
             <!-- name campo da inserire perchè ci facilità la gestione del campo come per esempio in questo caso il nome -->
-            <input type="text" class="form-control" id="writers" name="writers" value="{{ old (' writers' )}}">
+            <input type="text" class="form-control @error('writers') is-invalid @enderror" id="writers" name="writers" value="{{ old (' writers' )}}">
+        
+            <!-- inizio gestione errore del titlo con relativo messaggio sotto al campo  giusto -->
+            @error('writers')
+            <!-- direttiva error di blade -->
+            <div class="alert alert-danger ">
+                {{$message}}
+                <!-- questo campo si prende l'errore che abbiamo inserito nella nostra funzione validate nel controller -->
+            </div>
+    
+            @enderror
+            <!-- fine -->
+        
         </div>
         
         <!-- inizio campo di validazione -->
-        @if($errors->any())
+        <!-- @if($errors->any())
         <div class="alert alert-danger ">
             <ul>
                 @foreach($errors->all() as $error)
@@ -72,7 +167,7 @@ Dobbiamo inserirlo in ogni form. -->
             </ul>
         </div>
 
-        @endif
+        @endif -->
 
         <!-- fine campo di validazione -->
         
--- resources/views/comics/edit.blade.php
+++ resources/views/comics/edit.blade.php
@@ -16,53 +16,177 @@ Dobbiamo inserirlo in ogni form. -->
         <div class="mb-3">
             <label for="title" class="form-label text-uppercase">Nome del fumetto</label>
             <!-- name campo da inserire perchè ci facilità la gestione del campo come per esempio in questo caso il nome -->
-            <input type="text" class="form-control "   id="name" name="title" value="{{$comic->title}}">
+            <!-- abbiamo inserito @error('title')is invalid @enderror per gestire l'errore al di sotto del nostro campo del titolo e cosi sara anche negli altri campi -->
+            <input type="text" class="form-control @error('title')is-invalid @enderror"   id="name" name="title" value="{{$comic->title}}">
+
+            <!-- inizio gestione errore del titlo con relativo messaggio sotto al campo  giusto -->
+        @error('title')
+        <!-- direttiva error di blade -->
+        <div class="alert alert-danger ">
+            {{$message}}
+            <!-- questo campo si prende l'errore che abbiamo inserito nella nostra funzione validate nel controller -->
         </div>
+
+        @enderror
+        <!-- fine -->
+
+        </div>
+        
         <div class="mb-3">
             <label for="description" class="form-label text-uppercase">descriozione del fumetto</label>
             <!-- name campo da inserire perchè ci facilità la gestione del campo come per esempio in questo caso il nome -->
-            <textarea type="text" class="form-control" id="description" name="description"  >{{$comic->description}}</textarea>
+            <textarea type="text" class="form-control @error('description')is-invalid @enderror" id="description" name="description"  >{{$comic->description}}</textarea>
+
+            <!-- inizio gestione errore del titlo con relativo messaggio sotto al campo  giusto -->
+            @error('description')
+            <!-- direttiva error di blade -->
+            <div class="alert alert-danger ">
+                {{$message}}
+                <!-- questo campo si prende l'errore che abbiamo inserito nella nostra funzione validate nel controller -->
+            </div>
+    
+            @enderror
+            <!-- fine -->
+
         </div>
+
+
         <div class="mb-3">
             <label for="thumb" class="form-label text-uppercase ">link dell immagine del fumetto</label>
             <!-- name campo da inserire perchè ci facilità la gestione del campo come per esempio in questo caso il nome -->
-            <input type="url" class="form-control" id="thumb" name="thumb" value="{{$comic->thumb}}">
+            <input type="url" class="form-control @error('thumb')is-invalid @enderror" id="thumb" name="thumb" value="{{$comic->thumb}}">
         </div>
+
+        <!-- inizio gestione errore del titlo con relativo messaggio sotto al campo  giusto -->
+        @error('thumb')
+        <!-- direttiva error di blade -->
+        <div class="alert alert-danger ">
+            {{$message}}
+            <!-- questo campo si prende l'errore che abbiamo inserito nella nostra funzione validate nel controller -->
+        </div>
+
+        @enderror
+        <!-- fine -->
+
         <div class="mb-3">
             <label for="price" class="form-label text-uppercase ">prezzo del fumetto</label>
             <!-- name campo da inserire perchè ci facilità la gestione del campo come per esempio in questo caso il nome -->
-            <input type="text" class="form-control"  id="price" name="price" value="number" value="{{$comic->price}}">
+            <input type="text" class="form-control @error('price')is-invalid @enderror"  id="price" name="price" value="number" value="{{$comic->price}}">
+
+            <!-- inizio gestione errore del titlo con relativo messaggio sotto al campo  giusto -->
+            @error('price')
+            <!-- direttiva error di blade -->
+            <div class="alert alert-danger ">
+                {{$message}}
+                <!-- questo campo si prende l'errore che abbiamo inserito nella nostra funzione validate nel controller -->
+            </div>
+    
+            @enderror
+            <!-- fine -->
+
         </div>
+
+
         <div class="mb-3">
             <label for="series" class="form-label text-uppercase">serie del fumetto</label>
             <!-- name campo da inserire perchè ci facilità la gestione del campo come per esempio in questo caso il nome -->
-            <input type="text" class="form-control" id="series" name="series" value="{{$comic->series}}">
+            <input type="text" class="form-control @error('series')is-invalid @enderror" id="series" name="series" value="{{$comic->series}}">
+
+            <!-- inizio gestione errore del titlo con relativo messaggio sotto al campo  giusto -->
+            @error('series')
+            <!-- direttiva error di blade -->
+            <div class="alert alert-danger ">
+                {{$message}}
+                <!-- questo campo si prende l'errore che abbiamo inserito nella nostra funzione validate nel controller -->
+            </div>
+    
+            @enderror
+            <!-- fine -->
+
         </div>
+
+
         <!--  -->
         <div class="mb-3">
             <label for="sale_date" class="form-label text-uppercase">data di vendità</label>
             <!-- name campo da inserire perchè ci facilità la gestione del campo come per esempio in questo caso il nome -->
-            <input type="date" class="form-control" id="sale_date" name="sale_date" value="{{$comic->sale_date}}">
+            <input type="date" class="form-control @error('sale_date')is-invalid @enderror" id="sale_date" name="sale_date" value="{{$comic->sale_date}}">
+
+            <!-- inizio gestione errore del titlo con relativo messaggio sotto al campo  giusto -->
+            @error('sale_date')
+            <!-- direttiva error di blade -->
+            <div class="alert alert-danger ">
+                {{$message}}
+                <!-- questo campo si prende l'errore che abbiamo inserito nella nostra funzione validate nel controller -->
+            </div>
+    
+            @enderror
+            <!-- fine -->
+
         </div>
+
+
         <div class="mb-3">
             <label for="type" class="form-label text-uppercase">tipo</label>
             <!-- name campo da inserire perchè ci facilità la gestione del campo come per esempio in questo caso il nome -->
-            <input type="text" class="form-control" id="type" name="type" value="{{$comic->type}}">
+            <input type="text" class="form-control @error('type')is-invalid @enderror" id="type" name="type" value="{{$comic->type}}">
+
+            <!-- inizio gestione errore del titlo con relativo messaggio sotto al campo  giusto -->
+            @error('type')
+            <!-- direttiva error di blade -->
+            <div class="alert alert-danger ">
+                {{$message}}
+                <!-- questo campo si prende l'errore che abbiamo inserito nella nostra funzione validate nel controller -->
+            </div>
+    
+            @enderror
+            <!-- fine -->
+
         </div>
+
+
         <div class="mb-3">
             <label for="artists" class="form-label text-uppercase">artisti</label>
             <!-- name campo da inserire perchè ci facilità la gestione del campo come per esempio in questo caso il nome -->
-            <input type="text" class="form-control" id="artists" name="artists" value="{{$comic->artists}}">
+            <input type="text" class="form-control @error('artists')is-invalid @enderror" id="artists" name="artists" value="{{$comic->artists}}">
+
+            <!-- inizio gestione errore del titlo con relativo messaggio sotto al campo  giusto -->
+            @error('artists')
+            <!-- direttiva error di blade -->
+            <div class="alert alert-danger ">
+                {{$message}}
+                <!-- questo campo si prende l'errore che abbiamo inserito nella nostra funzione validate nel controller -->
+            </div>
+    
+            @enderror
+            <!-- fine -->
+
         </div>
+
+
         <div class="mb-3">
             <label for="writers" class="form-label text-uppercase">scrittori</label>
             <!-- name campo da inserire perchè ci facilità la gestione del campo come per esempio in questo caso il nome e per essere più precisi ci permette di modificarlo poi inseriamo value per avere il valore "salvato  Questo consente di mostrare i valori esistenti del fumetto nel formulario di modifica, consentendo all'utente di visualizzarli e modificarli se necessario." -->
-            <input type="text" class="form-control" id="writers" name="writers" value="{{$comic->writers}}">
+            <input type="text" class="form-control @error('writers') is-invalid @enderror" id="writers" name="writers" value="{{$comic->writers}}">
+
+            <!-- inizio gestione errore del titlo con relativo messaggio sotto al campo  giusto -->
+            @error('writers')
+            <!-- direttiva error di blade -->
+            <div class="alert alert-danger ">
+                {{$message}}
+                <!-- questo campo si prende l'errore che abbiamo inserito nella nostra funzione validate nel controller -->
+            </div>
+    
+            @enderror
+            <!-- fine -->
+            
         </div>
+
+
 
         <!-- inizio campo di validazione -->
         <!-- inserito per gestire la visualizzazione degli errori in pagina -->
-        @if($errors->any())
+        <!-- @if($errors->any())
         <div class="alert alert-danger ">
             <ul>
                 @foreach($errors->all() as $error)
@@ -74,7 +198,7 @@ Dobbiamo inserirlo in ogni form. -->
             </ul>
         </div>
 
-        @endif
+        @endif -->
 
         <!-- fine campo di validazione -->
         
